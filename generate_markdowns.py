016bee36828f51e7e4b7c1696a3210292d3ea3ce
Akshay <akshay.ap95@gmail.com>
2021-03-26 21:51:54 +0100

    Improve generate_markdowns.py script

--- generate_markdowns.py
+++ generate_markdowns.py
@@ -29,6 +29,7 @@ def generate_markdowns(path, output_dir):
     '''
     Iterates over each repository to build the .md files.
     '''
+    print("Generating markdowns for [{0}]".format(path))
     result = list(find_modules(path))
 
     skip_list = ['test']
@@ -43,13 +44,17 @@ def generate_markdowns(path, output_dir):
     markdowns_to_generate = list(markdowns_to_generate)
 
     for i in tqdm(range(len(markdowns_to_generate))):
-        command = 'pydoc-markdown -I {0} -m {1} --render-toc > markdowns/{2}/{3}.md' \
+
+        file_name = markdowns_to_generate[i].replace('.', '-') + '.md'
+
+        command = 'pydoc-markdown -I {0} -m {1} --render-toc > markdowns/{2}/{3}' \
                     .format(path, markdowns_to_generate[i],
-        output_dir,markdowns_to_generate[i])
+        output_dir,file_name)
+
         os.system(command)
 
 markdown_repos = [{'path':'aquarius/aquarius', 'output_dir': 'aquarius'},
-            {'path':'ocean.py/ocean_lib', 'output_dir': 'ocean.py'},
+            {'path':'ocean.py/ocean_lib', 'output_dir': 'ocean-py'},
             {'path':'provider/ocean_provider', 'output_dir': 'provider'}]
 
 for markdown_repo in markdown_repos:
